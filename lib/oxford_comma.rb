43cf6bec49526fe28adf93dcfb545b2430a53f37
Kevin Mathias <komathias1@gmail.com>
2020-03-26 21:33:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+if array.size = 1 
+  do array.join
+  elsif 
+  array.size = 2
+  do array.join("and")
+  elsif
+  array.size = 3
+  do 
+    if array.index = 0
+      puts "#{array(0)},"
+    else
+      puts "#{array(1)}, and"
+    end
+  else
+    if array.index >= 0
+      puts 
 end
